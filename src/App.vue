a73ef00cb0da2df79023da39cc7880abfc1d6e59
ttt <994777249@qq.com>
2024-06-28 17:51:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header></Header>
+    <footer></footer>
+    我是根组件
+  </div>
+</template>
+
+<script>
+import Header from './components/header'
+import footer from './components/footer'
+export default {
+  name: 'App',
+  components:{
+    Header,
+    footer
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/footer/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>foot组件</div>
+</template>
+
+<script>
+export default {
+    name:"footer"
+
+}
+</script>
+
+<style>
+
+</style>
