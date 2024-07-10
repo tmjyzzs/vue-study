92a5088b0d137ad0b4416b2b37fed62cde94eb5f
ttt <994777249@qq.com>
2024-07-10 17:59:17 +0800

    1

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,53 +1,25 @@
 <template>
   <div>
     <header class="header">
-      <!-- 头部的第一行 -->
+      <!-- 头部 -->
       <div class="top">
-        <div class="container">
-          <div class="loginList">
-            <p>尚品汇欢迎您！</p>
-            <!-- 没有登录：显示登录与注册 -->
-            <p>
-              <span>请</span>
-              <!-- <a href="###">登录</a> -->
-              <!-- 
-                声明式导航,当然你也可以使用编程式导航,因为不管是那个导航，都可以实现路由跳转，
-                但是最后为什么选择声明式导航，没有任何业务、逻辑
-               -->
-              <router-link to="/login">登录</router-link>
-              <router-link class="register" to="/register">注册</router-link>
-            </p>
-            <!-- 如果登录显示的是用户名字与退出登录 -->
-          </div>
-          <div class="typeList">
-            <router-link to="/center">我的订单</router-link>
-            <router-link to="/shopcart">我的购物车</router-link>
-            <a href="###">我的尚品汇</a>
-            <a href="###">尚品汇会员</a>
-            <a href="###">企业采购</a>
-            <a href="###">关注尚品汇</a>
-            <a href="###">合作招商</a>
-            <a href="###">商家后台</a>
-          </div>
-        </div>
+        <!-- 欢迎字段 -->
+        <p>急急急欢迎你</p>
+        <!-- 注册登入 -->
+        <p>
+          <span>请</span>
+          <a href="">请登入</a>
+          <a href="">免费注册</a>
+        </p>
+
+        <!-- 分类 -->
+        <div></div>
       </div>
-      <!--头部第二行 搜索区域-->
-      <div class="bottom">
-        <h1 class="logoArea">
-          <router-link class="logo" to="/home">
-            <img src="./images/logo.png" alt="" />
-          </router-link>
-        </h1>
-        <div class="searchArea">
-          <form action="###" class="searchForm">
-            <input type="text" id="autocomplete" class="input-error input-xxlarge" v-model="keyWord" />
-            <!-- 编程式导航:因为有业务 -->
-            <button class="sui-btn btn-xlarge btn-danger" type="button" @click="goSearch">
-              搜索
-            </button>
-          </form>
-        </div>
-      </div>
+
+      <!-- logo -->
+      <div></div>
+      <!-- 搜索 -->
+      <div></div>
     </header>
   </div>
 </template>
@@ -81,97 +53,5 @@ export default {
 </script>
 
 <style scoped lang="less">
-.header {
-  &>.top {
-    background-color: #eaeaea;
-    height: 30px;
-    line-height: 30px;
-
-    .container {
-      width: 1200px;
-      margin: 0 auto;
-      overflow: hidden;
-
-      .loginList {
-        float: left;
-
-        p {
-          float: left;
-          margin-right: 10px;
-
-          .register {
-            border-left: 1px solid #b3aeae;
-            padding: 0 5px;
-            margin-left: 5px;
-          }
-        }
-      }
-
-      .typeList {
-        float: right;
-
-        a {
-          padding: 0 10px;
-
-          &+a {
-            border-left: 1px solid #b3aeae;
-          }
-        }
-      }
-    }
-  }
-
-  &>.bottom {
-    width: 1200px;
-    margin: 0 auto;
-    overflow: hidden;
-
-    .logoArea {
-      float: left;
-
-      .logo {
-        img {
-          width: 175px;
-          margin: 25px 45px;
-        }
-      }
-    }
-
-    .searchArea {
-      float: right;
-      margin-top: 35px;
-
-      .searchForm {
-        overflow: hidden;
-
-        input {
-          box-sizing: border-box;
-          width: 490px;
-          height: 32px;
-          padding: 0px 4px;
-          border: 2px solid #ea4a36;
-          float: left;
-
-          &:focus {
-            outline: none;
-          }
-        }
-
-        button {
-          height: 32px;
-          width: 68px;
-          background-color: #ea4a36;
-          border: none;
-          color: #fff;
-          float: left;
-          cursor: pointer;
-
-          &:focus {
-            outline: none;
-          }
-        }
-      }
-    }
-  }
-}
+.header {}
 </style>
